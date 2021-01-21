9bee61035be98f494fcbe77368613adabcbccaa3
IlyaMurashkaSharekey <68434960+IlyaMurashkaSharekey@users.noreply.github.com>
2021-01-21 16:35:58 +0300

    [ch21688] increased timeout for selenoid

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { execSync } = require("child_process");
 
+const startupTimeout = process.env.SESSION_STARTUP_TIMEOUT || '60s';
+const attemptTimeout = process.env.SESSION_ATTEMPT_TIMEOUT || '60s';
+const deleteTimeout = process.env.SESSION_DELETE_TIMEOUT || '60s';
+const timeout = process.env.SESSION_TIMEOUT || '120s';
+
 console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
 
-execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start`)
+const args = `-service-startup-timeout ${startupTimeout} -session-attempt-timeout ${attemptTimeout} -session-delete-timeout ${deleteTimeout} -timeout ${timeout}`
+console.log(args);
+execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start --args "${args}"`)
 
 console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
